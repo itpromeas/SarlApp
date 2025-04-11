9fdaa8befda2120974b562f3f9935bcc220ae0c9
itpromeas <itpromeas@gmail.com>
2025-04-11 20:16:45 +0200

    category controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SarlApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(ILogger<CategoryController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
