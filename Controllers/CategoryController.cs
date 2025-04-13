5b8d9bdf4a207cbc72207103ae0d644f5b122514
itpromeas <itpromeas@gmail.com>
2025-04-13 02:51:18 +0200

    Edit

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -62,5 +62,40 @@ namespace SarlApp.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+                return NotFound();
+            
+            //CategoryModel? categoryFromDb = _db.Categories.Find(id);
+            CategoryModel? categoryFromDb = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //CategoryModel? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+            if (categoryFromDb == null)
+                return NotFound();
+            
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(CategoryModel obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        
     }
 }
--- tuto.md
+++ tuto.md
@@ -26,3 +26,25 @@ In Order to add javascript validation, we can use Validation partial script defi
         <partial name="_ValidationScriptsPartial" />
     }
 }
+
+# Find id in db
+
+The following are eqivalent:
+
+1. using Find() which uses the primary key of the model
+CategoryModel? categoryFromDb = _db.Categories.Find(id);
+
+2. Using FirstOrDefault or Where condition which use any field of the default
+CategoryModel? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+CategoryModel? categoryFromDb2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+
+
+Note: *id* in asp-route-id should match with parameter name *id* in *public IActionResult Edit(int? id)*
+
+If you use categoryId, then we will have
+
+asp-route-categoryId="@item.Id" 
+
+# edit
+
+add *<input asp-for="Id" hidden />* in order not to create a new category in edit
