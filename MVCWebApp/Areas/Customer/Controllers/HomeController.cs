994e8e427bb4dea99f79c0be429ddfcf0a49374e
Ange Salome Messeng Ekossono <itpromeas@gmail.com>
2025-04-25 13:25:56 +0200

    details in homepage

--- MVCWebApp/Areas/Customer/Controllers/HomeController.cs
+++ MVCWebApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.DataAccess.Repository.IRepository;
 using MVCWebApp.Models;
 
 namespace MVCWebApp.Areas.Customer.Controllers;
@@ -8,15 +9,24 @@ namespace MVCWebApp.Areas.Customer.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
     {
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
 
     public IActionResult Index()
     {
-        return View();
+        IEnumerable<ProductModel> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+        return View(productList);
+    }
+
+    public IActionResult Details(int productId) // this variable should match with the html asp-route-productId
+    {
+        ProductModel product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId, includeProperties: "Category");
+        return View(product);
     }
 
     public IActionResult Privacy()
