361599c021679d46c2cc91bd83cfa39387215d28
Ange Salome Messeng Ekossono <itpromeas@gmail.com>
2025-04-28 04:56:28 +0200

    DbInitializer

---
+++ MVCWebApp.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using MVCWebApp.DataAccess.Data;
+using MVCWebApp.Models;
+using MVCWebApp.Utility;
+
+namespace MVCWebApp.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly DbContextMVCSarl _db;
+
+        public DbInitializer(
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            DbContextMVCSarl db)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+            _db = db;
+        }
+
+
+        public void Initialize()
+        {
+            //migrations if they are not applied
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
+                    _db.Database.Migrate();
+                }
+            }
+            catch (Exception ex) { }
+
+
+
+            //create roles if they are not created
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
+
+
+                //if roles are not created, then we will create admin user as well
+                _userManager.CreateAsync(new SarlUser
+                {
+                    UserName = "admin@sarl.com",
+                    Email = "admin@sarl.com",
+                    Name = "Ange E.",
+                    PhoneNumber = "1112223333",
+                    StreetAddress = "test 123 Ave",
+                    State = "Bayern",
+                    PostalCode = "81456",
+                    City = "Munich"
+                }, "Admin123*").GetAwaiter().GetResult();
+
+
+                SarlUser user = _db.SarlUsers.FirstOrDefault(u => u.Email == "admin@sarl.com");
+                _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
+
+            }
+
+            return;
+        }
+    }
+}
--- MVCWebApp/Program.cs
+++ MVCWebApp/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using MVCWebApp.Utility;
 using Stripe;
+using MVCWebApp.DataAccess.DbInitializer;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -47,6 +48,9 @@ builder.Services.AddSession(options => {
     options.Cookie.IsEssential = true;
 });
 
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
 // razor page
 builder.Services.AddRazorPages();
 
@@ -77,6 +81,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.UseSession();
+SeedDatabase();
 
 app.MapRazorPages();
 
@@ -85,3 +90,12 @@ app.MapControllerRoute(
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+void SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        dbInitializer.Initialize();
+    }
+}
